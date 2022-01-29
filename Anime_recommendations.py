73fe09f566d9777a8d3e2a3ccf213d8387dd904b
DibyaSadhukhan <60581408+DibyaSadhukhan@users.noreply.github.com>
2022-01-30 03:25:23 +0530

    updated autocomplete form

---
+++ Anime_recommendations.py
@@ -0,0 +1,11 @@
+import pandas as pd
+class anime_details:
+    def __init__(self):
+        pass
+# returns the data of the recommmendations
+    def Get_anime_details(self,uid):
+        #takes the uid of the animes and returns the 
+        pass
+    def get_anime_recommendations(self,uid):
+        #get 10 recommendations for the searched anime.
+        pass
--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-
 #importing required packages
-from flask import Flask, render_template
-app = Flask(__name__, template_folder="Templates")
-@app.route('/', methods =['POST', 'GET'])
+from flask import Flask,Response,render_template,request
+import json
+from wtforms import Form,StringField
+import urllib.request 
 
+
+app = Flask(__name__, template_folder="Templates")
+app.config['SECRET_KEY']='Anime_recommender_2022_upvamp_secret_Code'
+
+class SearchForm(Form):
+    autocomp = StringField('Anime Name', id='anime_autocomplete',render_kw={"placeholder": "Please type in the name of the anime you have last seen"})
+ 
+ 
+@app.route('/_autocomplete', methods=['GET'])
+def autocomplete():
+    link="https://raw.githubusercontent.com/DibyaSadhukhan/Anime_recommender_data/main/Data/titles.txt"
+    response = urllib.request.urlopen(link)
+    Text = response.read()
+    Text = Text.decode("utf-8")
+    Text=Text.split('||')
+    return Response(json.dumps(Text), mimetype='application/json')
+ 
+
+@app.route('/', methods =['POST', 'GET'])
 def home():
-        return render_template('index.html',message="hi")
+        form = SearchForm(request.form)
+        print(form.autocomp.data)
+        return render_template('index.html',form=form)
+
+
+
 if __name__ == '__main__':
     app.run(debug = True)
