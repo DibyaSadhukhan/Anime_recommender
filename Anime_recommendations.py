631de902af48b6b2adb4d7fdc6960973383cd12e
DibyaSadhukhan <60581408+DibyaSadhukhan@users.noreply.github.com>
2022-02-06 23:07:09 +0530

    Version 1

--- Anime_recommendations.py
+++ Anime_recommendations.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import random
 class anime_recommendations:
 
     def Get_anime_UID(self,name):
@@ -14,13 +15,23 @@ class anime_recommendations:
         rec=rec.values[0]
         del data
         return rec  
-    def Get_recommendations_details(self,name):
+    def Get_recommendations_details(self,name,start=1,end=13):
         details=[]
         data=pd.read_csv('https://raw.githubusercontent.com/DibyaSadhukhan/Anime_recommender_data/main/Data/animes_details_cleaned.csv')
         rec=self.get_anime_recommendations(name)
-        for i in rec[1:13]:
-            detail=data.loc[data['uid'] == i]
-            details.append(dict(detail.iloc[0]))
+        detail=data.loc[data.uid.isin(rec[start:end])]
+        for i in range(len(detail)):
+            details.append(dict(detail.iloc[i]))
         del data
         del rec
+        del detail
+        return details
+    def Get_Random_details(self):
+        details=[]
+        data=pd.read_csv('https://raw.githubusercontent.com/DibyaSadhukhan/Anime_recommender_data/main/Data/animes_Home_page.csv')
+        detail=data.loc[data.index.isin(random.sample(range(random.randint(0,10000),15110),8))]
+        for i in range(len(detail)):
+            details.append(dict(detail.iloc[i]))
+        del data
+        del detail
         return details
--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def autocomplete():
 @app.route('/', methods =['POST', 'GET'])
 def home():
         form = SearchForm(request.form)
-        return render_template('base.html',form=form)
+        return render_template('Home.html',form=form)
 @app.route('/recommend', methods =['POST'])
 def recommend():
     form =SearchForm(request.form)
@@ -40,7 +40,9 @@ def recommend():
         details={}
         message="error"
     #return render_template('index.html',form=form)
-    return render_template('index.html',form=form,data=details,message=message)
-
+    return render_template('Recommend.html',form=form,data=details,message=message)
+@app.route('/features.txt', methods=['GET'])
+def features():
+    return '<object data="./features.txt" width="100vw" height="100vh">Not supported</object>'
 if __name__ == '__main__':
     app.run(debug = True)
